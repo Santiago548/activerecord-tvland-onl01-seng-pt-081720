dd3bebc2fd0fe7397b21badcadb9dee02c8c2ec9
Dominic Barajas <dominicbarajas548@gmail.com>
2020-10-31 19:48:30 +0000

    Automatically backed up by Learn

--- db/migrate/20201031194700_update_shows_table.rb
+++ db/migrate/20201031194700_update_shows_table.rb
@@ -1,4 +1,7 @@
 class UpdateShowsTable < ActiveRecord::Migration[5.2]
   def change
+    add_column :shows, :day, :string
+    add_column :genre, :day, :string
+    add_column :season, :day, :string
   end
 end
